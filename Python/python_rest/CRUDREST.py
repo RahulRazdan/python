ad9a670be53eb096ea789d0ca714d0e4f25175b4
RahulRazdan <razdanrahul0@gmail.com>
2018-07-20 17:06:03 +0530

    complete CRUD operation

--- Python/python_rest/CRUDREST.py
+++ Python/python_rest/CRUDREST.py
@@ -28,7 +28,7 @@ class User(db.Model):
 class UserSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ('username','email')
+        fields = ('id','username','email')
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
